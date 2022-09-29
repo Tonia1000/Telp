da3baaa361ce90a788e7e3394da61377da73abbc
Tonia1000 <99226928+Tonia1000@users.noreply.github.com>
2022-09-29 17:08:25 -0300

    commi

--- Go-Alex/GO/ex6.go
+++ Go-Alex/GO/ex6.go
@@ -12,7 +12,7 @@ func media() {
 	n1 := 0
 	n2 := 0
 	result := 0
-	fmt.Printf("Digite a operação desejada: ")
+	fmt.Printf("Digite a operação desejada sem acento: ")
 	fmt.Scan(&op)
 
 	fmt.Printf("Digite o primeiro numero: ")
@@ -20,9 +20,20 @@ func media() {
 	fmt.Printf("Digite o segundo numero: ")
 	fmt.Scan(&n2)
 
-	if op == soma {
+	if op == "soma" {
+		result = n1+n2
+
+	}
+	if op == "divisao"{
+		result = n1/n2
+
+	}
+	if op == "multiplicacao"{
+		result == n1*n2
 
 	}
 
 	fmt.Printf("A media das %d notas é igual a %d.\n", n, m)
 }
+
+// https://go.dev/play/
