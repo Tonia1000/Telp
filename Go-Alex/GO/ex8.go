831b2ec77d7a650f983fd451e6b6ee5ad560f182
Tonia1000 <99226928+Tonia1000@users.noreply.github.com>
2022-10-15 19:39:07 -0300

    commit

--- Go-Alex/GO/ex8.go
+++ Go-Alex/GO/ex8.go
@@ -36,3 +36,39 @@ func vogal() {
 
 	fmt.Printf("A palavra digitada sem vogais é %s",)
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"strings"
+// )
+
+// func filter(data []string, f func(string) bool) []string {
+
+// 	fltd := make([]string, 0)
+
+// 	for _, e := range data {
+
+// 		if f(e) {
+// 			fltd = append(fltd, e)
+// 		}
+// 	}
+
+// 	return fltd
+// }
+
+// func main() {
+
+// 	words := []string{"war", "water", "cup", "tree", "storm"}
+
+// 	p := "w"
+
+// 	res := filter(words, func(s string) bool {
+
+// 		return strings.HasPrefix(s, p)
+// 	})
+
+// 	fmt.Println(res)
+
+// }
